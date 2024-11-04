6b7173bc402be22e558febd6725b3b3b1d9c8bdc
jjeeccss <jeicogacusana24@gmail.com>
2024-11-04 22:59:48 +0800

    initial commit

---
+++ colorPref.js
@@ -0,0 +1,11 @@
+// Create an array to store colors
+let colors = ["red", "yellow", "green"];
+
+// Print the initial array
+console.log("Your favorite colors are:", colors);
+
+// Add a new color to the array
+colors.push("red");
+
+// Print the updated array
+console.log("Updated favorite colors:", colors);
